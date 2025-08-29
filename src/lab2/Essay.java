3f7b7643ec439ef11e280c40c5dfd005048c2c39
Quentin Proulx <6324569@edu.vaniercollege.qc.ca>
2025-08-29 12:56:51 -0400

    Finished Essay class

--- src/lab2/Essay.java
+++ src/lab2/Essay.java
@@ -8,6 +8,48 @@ package lab2;
  *
  * @author 6324569
  */
-public class Essay {
+public class Essay extends GradedActivity {
+    private double grammer;
+    private double spelling;
+    private double correctLength;
+    private double content;
     
+    public void setScore(double gr, double sp, double len, double cnt) {
+        this.grammer = gr;
+        this.spelling = sp;
+        this.correctLength = len;
+        this.content = cnt;
+    }
+    
+    public void setGrammar(double g) {
+        this.grammer = g;
+    }
+    
+    public void setSpelling(double s) {
+        this.spelling = s;
+    }
+    
+    public void setCorrectLength(double c) {
+        this.correctLength = c;
+    }
+    
+    public void setContent(double c) {
+        this.content = c;
+    }
+
+    public double getGrammer() {
+        return grammer;
+    }
+
+    public double getSpelling() {
+        return spelling;
+    }
+
+    public double getCorrectLength() {
+        return correctLength;
+    }
+    
+    public double getScore() {
+        return grammer + spelling + correctLength + content;
+    }
 }
